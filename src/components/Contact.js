73d36bd04cebdb2c7c8f73f7bbfa128aa81b9154
JBuhay <jbuhay3@gmail.com>
2020-03-25 11:55:17 -0700

    Contact me

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,53 +5,12 @@ export class Contact extends Component {
 
     constructor(props) {
         super(props);
-        this.handleSubmit = this.handleSubmit.bind(this);
     }
-
-    handleSubmit (event) {
-        event.preventDefault()
-
-        const {
-          REACT_APP_EMAILJS_RECEIVER: receiverEmail,
-          REACT_APP_EMAILJS_TEMPLATEID: template,
-          REACT_APP_EMAILJS_USERID: user
-        } = this.props.env
-
-        this.sendFeedback(
-          template,
-          this.props.senderEmail,
-          receiverEmail,
-          this.state.feedback,
-          user
-        )
-
-        this.setState({
-          formSubmitted: true
-        })
-      }
-
-      sendFeedback (templateId, senderEmail, receiverEmail, feedback, user) {
-        window.emailjs.send(
-          'default_service', // default email provider in your EmailJS account
-          templateId,
-          {
-            senderEmail,
-            receiverEmail,
-            feedback
-          },
-          user
-        )
-          .then(res => {
-            this.setState({ formEmailSent: true })
-          })
-          // Handle errors here however you like, or use a React error boundary
-          .catch(err => console.error('Failed to send feedback. Error: ', err))
-      };
 
   render() {
     return (
       <div className="b3">
-        <form
+        {/* <form
           onSubmit={this.handleSubmit}
           method="POST"
         >
@@ -71,12 +30,12 @@ export class Contact extends Component {
             <label htmlFor="message">Message</label>
             <textarea className="form-control" rows="5"></textarea>
           </div>
-          {/* <button type="submit" className="SubmitButton" onSubmit={this.handleSubmit}>
-          </button> */}
-          <a href="mailto:jbuhay3@gmail.com">Submit</a>
-            {/* Submit */}
+          <button type="submit" className="SubmitButton" onSubmit={this.handleSubmit}>
+          </button>
+             Submit
 
-        </form>
+        </form> */}
+            <a className="SubmitButton" href="mailto:jbuhay3@gmail.com">Contact Me</a>
       </div>
     );
   }
