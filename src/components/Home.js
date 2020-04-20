661df3ad8339ea26b8cecf33ab7b0bcade849994
JBuhay <jbuhay3@gmail.com>
2020-04-20 01:35:04 -0700

    Adding images

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,6 +7,14 @@ import { Link} from "react-scroll";
 import arrow from "../images/ArrowDesign.svg";
 import design1 from "../images/Design_1.svg";
 import design2 from "../images/Design_2.svg";
+import ucdlogo from "../images/ucdlogo.png";
+import ucdname from "../images/ucdname.png";
+import csmlogo from "../images/csm.png";
+import downwardarrow from "../images/downwardarrow.svg";
+import justin from "../images/justin.png";
+
+
+
 
 export class Home extends Component {
   render() {
@@ -26,6 +34,7 @@ export class Home extends Component {
             Hi, I’m a Web and Mobile Developer based in San Francisco,
             California.
           </h2>
+          <img src={justin} id="justin" alt="justin" />
           <h3>Scroll Down</h3>
           <Link
             className="Arrow"
@@ -43,18 +52,25 @@ export class Home extends Component {
         </div>
 
         <div className="bhome2">
-          <h2 className="First">
-            Went to school at University of California, Davis for a Computer
-            Science Degree
-          </h2>
-          <h2>
-            Have an A.S. Degree for Computer and Information Science from
-            College of San Mateo
-          </h2>
-          <h2>
-            Created various amount of projects throughout my courses. Also
-            creating other projects on my own time.
-          </h2>
+          <div className="textbhome2">
+            <h1>About Me</h1>
+            <h2>
+            I'm currently a 4th year student at University of California, Davis pursuing a B.S. in Computer science.
+            </h2>
+            <h2>
+            I've transferred from College of San Mateo with an A.S in Computer and Information Science as Magna Cum Laude.
+            </h2>
+            <h2>
+            The goals and interest I have for my career would to become an Applications Software Developer and creating applications that can help the world.
+
+            </h2>
+          </div>
+          <div className="imagesbhome2">
+            <img src={csmlogo} id="csmlogo" alt="csmlogo"/>
+            <img src={downwardarrow} id="downwardarrow" alt="arrowlogo"/>
+            <img src={ucdlogo} alt="ucdlogo"/>
+            <img src={ucdname} id="imgname" alt="ucdname"/>
+          </div>
         </div>
       </div>
     );
